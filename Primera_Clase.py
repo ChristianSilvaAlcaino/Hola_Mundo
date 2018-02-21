53d4b25295c5d694c2a6ddf773a0b26ee10ac186
Christian Silva Alcaíno <33494721+ChristianSilvaAlcaino@users.noreply.github.com>
2018-02-21 00:44:44 -0300

    Add files via upload

---
+++ Primera_Clase.py
@@ -0,0 +1,43 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>> print ("Christian")
+Christian
+>>> 56+78
+134
+>>> 30%3
+0
+>>> 100&50
+32
+>>> 678**
+  File "<stdin>", line 1
+    678**
+        ^
+SyntaxError: invalid syntax
+>>> 678**5
+143267759542368
+>>> 25%5
+0
+>>> numero=5
+>>> type (numero)
+<class 'int'>
+>>> nombre="Christian"
+>>> type(nombre)
+<class 'str'>
+>>> mensaje="""Este es un mensaje con 
+... 3 saltos de 
+... linea 
+... """
+>>> print(mensaje)
+Este es un mensaje con 
+3 saltos de 
+linea 
+
+>>> numero1=5
+>>> numero2=7
+>>> if numero1>numero2:
+... 	print("El numero 1 es mayor")
+... else:
+... 	print("El numero 2 es mayor")
+... 
+El numero 2 es mayor
+>>> 
